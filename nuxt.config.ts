350695da8ca5c71c9508b57abd0c8d588ba4aa97
ianahmfac <me.business25@gmail.com>
2024-09-02 19:17:08 +0700

    Add nuxt content & tailwind prose to show markdown content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@nuxt/content",
+  ],
   colorMode: {
     classSuffix: "",
   },
---
+++ pages/blog/[...slug].vue
@@ -0,0 +1,15 @@
+<!-- penambahan [...slug] untuk keperluan pembacaan url mendalam -->
+<!-- Contohnya mengambil dari path: content/blog/markdown.md -->
+<!-- Ketika kita mencari blog/markdown, maka akan ditrace sampai ke dalam" folder atau url -->
+
+<script setup>
+useHead({
+  title: "Blog",
+});
+</script>
+
+<template>
+  <article class="prose dark:prose-invert">
+    <ContentDoc />
+  </article>
+</template>
