6343bc8c24ad63f969aa59255b3209af1dcf3cac
ianahmfac <me.business25@gmail.com>
2024-09-03 20:18:59 +0700

    Implement component on markdown file

--- components/content/BlogList.vue
+++ components/content/BlogList.vue
@@ -1,16 +1,17 @@
 <script setup>
-useHead({
-  title: "Blog",
-});
-
 const { data: posts } = await useAsyncData("blog-list", () => {
-  return queryContent("blog").only(["_path", "title"]).find();
+  return (
+    queryContent("blog")
+      //   Exclude blog.md post
+      .where({ _path: { $ne: "/blog" } })
+      .only(["_path", "title"])
+      .find()
+  );
 });
 </script>
 
 <template>
-  <section class="prose dark:prose-invert">
-    <h1 class="text-2xl font-bold mb-10">Blog</h1>
+  <section class="not-prose">
     <ul>
       <li v-for="post in posts" :key="post._path">
         <NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
---
+++ content/blog/index.md
@@ -0,0 +1,10 @@
+---
+title: "Blog"
+description: "List of all my recent blog posts"
+---
+
+# Blog Post List
+
+List of all my recent blog posts
+
+::BlogList
