afc9b4b3a1914355fadc0842633de85c6dc4635a
ianahmfac <me.business25@gmail.com>
2024-09-04 20:30:18 +0700

    Add table of content of markdown

---
+++ components/TocLink.vue
@@ -0,0 +1,27 @@
+<script setup>
+const route = useRoute();
+defineProps({
+  links: Array,
+  level: {
+    type: Number,
+    default: 0,
+  },
+});
+</script>
+
+<template>
+  <ul>
+    <li v-for="link in links" :key="link.id">
+      <NuxtLink
+        :to="{ path: route.path, hash: `#${link.id}` }"
+        class="hover:font-semibold hover:underline"
+        :class="{
+          'ml-4': level > 0,
+        }"
+      >
+        {{ link.text }}
+      </NuxtLink>
+      <TocLink v-if="link.children" :links="link.children" :level="level + 1" />
+    </li>
+  </ul>
+</template>
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -13,6 +13,23 @@ useHead({
   <article
     class="content-doc-full prose-pre:bg-gray-50 dark:prose-pre:bg-slate-900 prose-pre:text-slate-800 dark:prose-pre:text-gray-100"
   >
-    <ContentDoc />
+    <ContentDoc v-slot="{ doc }">
+      <div class="grid md:grid-cols-8 grid-cols-1 md:gap-16 gap-2">
+        <div class="md:col-span-6 col-auto">
+          <!-- Menampilkan content isi dari doc -->
+          <ContentRenderer :value="doc" />
+        </div>
+
+        <!-- Menampilkan Table of Content -->
+        <div class="md:col-span-2 not-prose col-auto">
+          <aside class="sticky top-8">
+            <div class="font-bold mb-2">Table of Content</div>
+            <nav>
+              <TocLink :links="doc.body.toc.links" />
+            </nav>
+          </aside>
+        </div>
+      </div>
+    </ContentDoc>
   </article>
 </template>
